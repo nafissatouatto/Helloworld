cfbca30e289f0e41c93c564b21fefbc35db90725
nafissa <dian_83@live.fr>
2019-04-23 16:51:28 +0200

    ajout methodes dans la classe order

--- src/com/prj/projet/Order.java
+++ src/com/prj/projet/Order.java
@@ -6,7 +6,11 @@ public class Order {
      *Affichage des menus
      */
     public void displaylabelMenu () {
-
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
     }
 
     /**
@@ -14,6 +18,24 @@ public class Order {
      */
     public void displaySelectedMenu () {
 
+
+        /**
+         * Display all available menus in the restaurant.
+         */
+
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
+    }
+            /**
+             * Display a selected menu.
+             * @param nbMenu The selected menu.
+             */
+            public void displaySelectedMenu(int nbMenu) {
+                System.out.println("Vous avez choisi le menu " + nbMenu);
+            }
+
     }
 
-}
